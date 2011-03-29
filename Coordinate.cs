114cc45dafb4adfe4f1f8ad28048967b1e53fa1e
Daniel Kuwahara <dkuwahara@gmail.com>
2011-03-29 12:58:36 -0400

    - Added new classes for in game stuff

---
+++ Coordinate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpClient
+{
+    class Coordinate
+    {
+        protected Int32 m_x;
+        public Int32 X
+        {
+            get { return m_x; }
+            set { m_x = value; }
+        }
+
+        protected Int32 m_y;
+        public Int32 Y
+        {
+            get { return m_y; }
+            set { m_y = value; }
+        }
+
+        public Coordinate()
+        {
+
+        }
+
+        public Coordinate(Int32 x, Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+
+        public static bool operator==(Coordinate first, Coordinate second)
+        {
+            return (first.X == second.X) && (first.Y == second.Y);
+        }
+
+        public Double Distance(Coordinate other)
+        {
+            Double x2 = Math.Pow(m_x - other.X, 2.0);
+            Double y2 = Math.Pow(m_y - other.Y, 2.0);
+            Double distance = Math.Sqrt(x2 + y2);
+            return distance;
+        }
+
+    }
+}
---
+++ GameData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpClient
+{
+    class GameData
+    {
+        protected Boolean m_fullyEnteredGame;
+        public Boolean FullyEnteredGame
+        {
+            get { return m_fullyEnteredGame; }
+            set { m_fullyEnteredGame = value; }
+        }
+
+        protected Int32 m_lastTeleport;
+        public Int32 LastTeleport
+        {
+            get { return m_lastTeleport; }
+            set { m_lastTeleport = value; }
+        }
+
+        protected Int32 m_experience;
+        public Int32 Experience
+        {
+            get { return m_experience; }
+            set { m_experience = value; }
+        }
+
+    }
+}
