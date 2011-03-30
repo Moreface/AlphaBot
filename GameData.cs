d2ccd0ae79998c257ba8d4496083154aa6a273d1
Daniel Kuwahara <dkuwahara@gmail.com>
2011-03-29 22:20:03 -0400

    - Added lots of game data properties - Added ActType Enum

--- GameData.cs
+++ GameData.cs
@@ -7,26 +7,149 @@ namespace CSharpClient
 {
     class GameData
     {
-        protected Boolean m_fullyEnteredGame;
-        public Boolean FullyEnteredGame
+        /*
+         * 
+         *  Enumerations and Static Consts
+         * 
+         * 
+         */
+	    public enum CharacterClassType
+	    {
+		    AMAZON,
+		    SORCERESS,
+		    NECROMANCER,
+		    PALADIN,
+		    BARBARIAN,
+		    DRUID,
+		    ASSASSIN
+	    };
+
+        public enum ActType
         {
-            get { return m_fullyEnteredGame; }
-            set { m_fullyEnteredGame = value; }
+            ACT_I = 0,
+            ACT_II = 1,
+            ACT_III = 2,
+            ACT_IV = 3,
+            ACT_V = 4
+        };
+
+        public const Int32 InventoryWidth = 10;
+        public const Int32 InventoryHeight = 4;
+
+        public const Int32 StashWidth = 6;
+        public const Int32 StashHeight = 8;
+
+        public const Int32 CubeWidth = 3;
+        public const Int32 CubeHeight = 4;
+
+
+        public GameData()
+        {
+            SkillLevels = new Dictionary<Skills.Type, uint>();
+            ItemSkillLevels = new Dictionary<Skills.Type, uint>();
+            Players = new Dictionary<uint,Player>();
+            Npcs = new Dictionary<uint,NpcEntity>();
+            Items = new Dictionary<uint, ItemType>();
         }
+        /*
+         * 
+         * Members and Properties
+         * 
+         */
+        protected ActType m_currentAct;
+        public ActType CurrentAct { get { return m_currentAct; } set { m_currentAct = value; } }
+
+        protected Int32 m_mapId;
+        public Int32 MapId { get { return m_mapId; } set { m_mapId = value; } }
+        protected Int32 m_areaId;
+        public Int32 AreaId { get { return m_areaId; } set { m_areaId = value; } }
+
+        protected Boolean m_fullyEnteredGame;
+        public Boolean FullyEnteredGame { get { return m_fullyEnteredGame; } set { m_fullyEnteredGame = value; } }
+
+        protected Boolean m_talkedToNpc;
+        public Boolean TalkedToNpc { get { return m_talkedToNpc; } set { m_talkedToNpc = value; } }
+
 
         protected Int32 m_lastTeleport;
-        public Int32 LastTeleport
-        {
-            get { return m_lastTeleport; }
-            set { m_lastTeleport = value; }
-        }
+        public Int32 LastTeleport { get { return m_lastTeleport; } set { m_lastTeleport = value; } }
 
-        protected Int32 m_experience;
-        public Int32 Experience
-        {
-            get { return m_experience; }
-            set { m_experience = value; }
-        }
+        protected UInt32 m_experience;
+        public UInt32 Experience { get { return m_experience; } set { m_experience = value; } }
 
+        protected Player m_me;
+        public Player Me { get { return m_me; } set { m_me = value; } }
+
+        private Dictionary<Skills.Type, UInt32> m_skillLevels;
+        public Dictionary<Skills.Type, UInt32> SkillLevels { get { return m_skillLevels; } set { m_skillLevels = value; } } 
+
+        private Dictionary<Skills.Type, UInt32> m_itemSkillLevels;
+        public Dictionary<Skills.Type, UInt32> ItemSkillLevels { get { return m_itemSkillLevels; }set { m_itemSkillLevels = value; } }
+
+        private Dictionary<UInt32, Player> m_players;
+        public Dictionary<UInt32, Player> Players { get { return m_players; } set { m_players = value; } }
+
+        private Dictionary<UInt32, NpcEntity> m_npcs;
+        public Dictionary<UInt32, NpcEntity> Npcs { get { return m_npcs; } set { m_npcs = value; } }
+
+        private Dictionary<UInt32, ItemType> m_items;
+        public Dictionary<UInt32, ItemType> Items { get { return m_items; } set { m_items = value; } }
+
+        private Container m_inventory;
+        public Container Inventory { get { return m_inventory; } set { m_inventory = value; } }
+        
+        private Container m_stash;
+        public Container Stash { get { return m_stash; } set { m_stash = value; } }
+        
+        private Container m_cube;
+        public Container Cube { get { return m_cube; } set { m_cube = value; } }
+
+        private Container m_belt;
+        public Container Belt { get { return m_belt; } set { m_belt = value; } }
+
+        private Int32 m_malahId;
+        public Int32 MalahId { get { return m_malahId; } set { m_malahId = value; } }
+
+        private Int32 m_currentLife;
+        public Int32 CurrentLife { get { return m_currentLife; } set { m_currentLife = value; } }
+
+        protected Boolean m_firstNpcInfoPacket;
+        public Boolean FirstNpcInfoPacket { get { return m_firstNpcInfoPacket; } set { m_firstNpcInfoPacket = value; } }
+
+        private Int32 m_attackSinceLastTeleport;
+        public Int32 AttacksSinceLastTeleport { get { return m_attackSinceLastTeleport; } set { m_attackSinceLastTeleport = value; } }
+
+        private Int32 m_weaponSet;
+        public Int32 WeaponSet { get { return m_weaponSet; } set { m_weaponSet = value; } }
+
+        protected Boolean m_hasMerc;
+        public Boolean HasMerc { get { return m_hasMerc; } set { m_hasMerc = value; } }
+
+        /*
+         * 
+         * Methods
+         * 
+         */
+        public Player GetPlayer(UInt32 id)
+        {
+            if (id == m_me.Id)
+                return m_me;
+            else
+            {
+                Player temp;
+                bool success = m_players.TryGetValue(id,out temp);
+
+                if (success)
+                    return temp;
+                else
+                {
+                    m_players.Add(id, new Player());
+                    m_players[id].Id = id;
+                    return m_players[id];
+                }
+            }
+
+                
+        }
     }
 }
