fed228aa4e7aa536ef2627008bfea3a4f53e0077
Daniel Kuwahara <dkuwahara@gmail.com>
2011-04-02 17:32:08 -0400

    - Added comparer, should be temporary, will switch to lord2800's suggestion for new pickit

---
+++ ItemEntryComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpClient
+{
+    class ItemEntryComparer : IEqualityComparer<ItemType>
+    {
+        public bool Equals(ItemType i1, ItemType i2)
+        {
+            if (i1.quality == i2.quality && i1.type == i2.type)
+            {
+                if (((i1.has_sockets == false) && (i2.has_sockets == false || i2.sockets == uint.MaxValue)) || i1.sockets == i2.sockets)
+                    return true;
+            }
+            if (i1.type == "rv1")
+                return true;
+            return false;
+        }
+
+
+        public int GetHashCode(ItemType ix)
+        {
+            uint hCode = ix.sockets;
+            return hCode.GetHashCode();
+        }
+    }
+}
